0d032c56ae1acd7bea1e59174cad1d12fa3650af
Kiadtisak-Notebook <Kiadtisak@ubu.ac.th>
2024-09-05 10:07:41 +0700

    Lab 11 Bike Project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         List<Bike> bikes = new ArrayList<Bike>();
         Bike bike = new Bike();
         bike.setBike_name("Specialized S-Works Aethos");
-        bike.setBike_image("https://s14761.pcdn.co/wp-content/uploads/sites/3/2019/06/Cannondale-SuperSix-Evo-2020-Rennrad-Road-Bike-Test-Review-26-1140x760.jpg");
+        bike.setBike_image("https://www.duckingtiger.com/wp-content/uploads/2020/10/Specialized-S-Works-Aethos-Driveside_3_4-Copy.jpg");
         bike.setBike_price(478047);
         bikes.add(bike);
 
@@ -47,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
         bike3.setBike_image("https://www.serviziocorsa.com/media/image/product/25100/md/en-bianchi-aria-campagnolo-chorus-2021.jpg");
         bike3.setBike_price(150900);
         bikes.add(bike3);
+
+        Bike bike4 = new Bike();
+        bike4.setBike_name("ริวบิดซิ่ง");
+        bike4.setBike_image("https://scontent-bkk1-2.xx.fbcdn.net/v/t39.30808-6/273658113_2115556781937846_2574392847367048890_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=485ouEJ2BkQQ7kNvgExNG7B&_nc_ht=scontent-bkk1-2.xx&_nc_gid=ANrMXggdOePpXP-TfCXvlzq&oh=00_AYC4S3rs_QeAHm44j-y390wULVNK3MyquXG3oDYdcdlRpQ&oe=66DED912");
+        bike4.setBike_price(10000);
+        bikes.add(bike4);
 
 
         // ส่ง bikes ไปให้ MyAdapter
